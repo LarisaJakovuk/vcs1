be55763369fe2f90cf5e7885ce95eaa70b22657c
Larisa <66107818+LarisaJakovuk@users.noreply.github.com>
2020-06-01 20:14:24 +0300

    Add files via upload

---
+++ VcsWebdriver/Pages/PageBase.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+
+namespace VcsWebdriver.Pages
+{
+    public class PageBase
+    {
+        protected static IWebDriver Driver;
+
+        public PageBase(IWebDriver webdriver)
+        {
+            Driver = webdriver;
+        }
+
+        public void CloseBrowser()
+        {
+            Driver.Quit();
+        }
+    }
+}
